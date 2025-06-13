959a7e4a9c92bf37a84e828e0b1757f0c3b877da
Pujan Joshi <pujan.joshi@pfizer.com>
2025-06-13 14:45:21 -0400

    Update math.py
    added comment

--- utils/math.py
+++ utils/math.py
@@ -5,10 +5,11 @@ import scipy.stats as stats
 
 def generate_random_floats(length: int = 1000, range_upper: int = 5000):
     """
-    Generate a list of random floats between 0 and range_upper (default: 5000)
+    Generate a list of random floats between 0 and range_upper
 
     Parameters:
     length (int): The number of random floats to generate.
+    range_upper (int): The upper limit for the random float values.
 
     Returns:
     list: A list of random float numbers.
